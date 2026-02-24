10c93416b667f17f27dcde951ab9a26b346f35b0
Joseph Boyd <josephboyd113@gmail.com>
2026-02-24 16:21:21 -0600

    Added ability to specify more than one external domain. Added option for unregistered domain

--- Test_Direct_Send_Spoofing.ps1
+++ Test_Direct_Send_Spoofing.ps1
@@ -7,27 +7,47 @@
         [Parameter(Mandatory = $true)] [string]$from_valid_mailbox,
         [Parameter(Mandatory = $true)] [string]$from_invalid_mailbox,
         [Parameter(Mandatory = $true)] [string]$from_external_domain,
+        [Parameter(Mandatory = $true)] [string]$from_unregistered_domain,
 
         [string]$body = "This is a test. Please notify the tester if you receive this message.",
 
         [string]$sleep_interval = 5
     )
+    $subject_number = 1
 
-    $subject = "DS Test 1: Sent from valid mailbox"
+    $subject = "DS Test $subject_number: Sent from valid mailbox"
     echo "Sending: '$subject'..."
     Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_valid_mailbox -To $to
 
     start-sleep $sleep_interval
 
-    $subject = "DS Test 2: Sent from non-existent mailbox"
+    $subject_number = $subject_number + 1
+    $subject = "DS Test $subject_number: Sent from non-existent mailbox"
     echo "Sending: '$subject'..."
     Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_invalid_mailbox -To $to
 
     start-sleep $sleep_interval
 
-    $subject = "DS Test 3: Sent from external domain"
+    if ($from_external_domain.Contains(",")) {
+        $external_address_array = $from_external_domain -split ","
+        foreach ($external_address in $external_address_array) {
+            $subject_number = $subject_number + 1
+            $subject = "DS Test $subject_number: Sent from external domain"
+            echo "Sending: '$subject'..."
+            Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $external_address -To $to
+        }
+    } else {   
+        $subject_number = $subject_number + 1
+        $subject = "DS Test $subject_number: Sent from external domain"
+        echo "Sending: '$subject'..."
+        Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_external_domain -To $to
+    }
+
+
+    $subject_number = $subject_number + 1
+    $subject = "DS Test $subject_number: Sent from external domain"
     echo "Sending: '$subject'..."
-    Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_external_domain -To $to
-    
+    Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_unregistered_domain -To $to
+
     echo "All done!"
 }
